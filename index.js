937656b70f2dfbdeddc7f2623d23e7b9a3bb5700
vsh <sijiavsh@outlook.com>
2021-11-12 20:40:58 +0100

    one round done

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-
 // return rock paper scissor randomly
 const rps = ["Rock", "Paper", "Scissors"]
-let computerPlay = rps[Math.floor(Math.random()*3)]
+const computerPlay = () => rps[Math.floor(Math.random()*3)]
+//console.log(computerPlay)
+function playRound(playerSelection, computerSelection){
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+    if (player == computer){
+        return "Tie"
+    } else if ((player == "rock" && computer == "scissors") 
+    || (player == "paper" && computer == "rock")
+    || (player == "scissors" && computer == "paper")){
+        return "You win!"
+    } else {
+        return "You lose!"
+    }
+    }
 
-
-console.log(computerPlay)
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
